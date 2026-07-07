e69870779baa4bb7469477cd11135e8f73893af0
Benjamin Pabst <166110368+BenjaminPabst@users.noreply.github.com>
2026-07-07 07:56:20 +0200

    fix: standalone

--- src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -16,7 +16,9 @@ interface DashboardPanel {
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  // eslint-disable-next-line @angular-eslint/prefer-standalone
+  standalone: false
 })
 export class DashboardComponent {
   viewModel$: Observable<DashboardViewModel>
